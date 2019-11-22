1bd4674986cd78847eaf09be3fb62d382bce14fb
varbogas <55903701+varbogas@users.noreply.github.com>
2019-11-21 20:21:21 -0600

    Finished studio.

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,6 +12,81 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsAssertEquals() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertEquals(true, test_Class.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertTrue(test_Class.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrueTwo() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertTrue(test_Class.hasBalancedBrackets("[{[ ]}]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrueThree() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertTrue(test_Class.hasBalancedBrackets(" [ ] "));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrueFour() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertTrue(test_Class.hasBalancedBrackets(" [[2 ]3] "));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalse() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertFalse(test_Class.hasBalancedBrackets("[]]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalseTwo() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertFalse(test_Class.hasBalancedBrackets("[}"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalseThree() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertFalse(test_Class.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalseFour() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertFalse(test_Class.hasBalancedBrackets("{{ ]}}"));
+    }
+
+
+    @Test
+    public void onlyBracketsReturnsNotNull() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertNotNull(test_Class.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void onlyBracketsAssertSame() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertSame(test_Class.hasBalancedBrackets("[]"), test_Class.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void onlyBracketsAssertNotSame() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertNotSame(test_Class.hasBalancedBrackets("[]]"), test_Class.hasBalancedBrackets("[[]]"));
+    }
+
+
 
 
 }
